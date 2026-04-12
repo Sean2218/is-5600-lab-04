9ea876b2e3b4742fecb20065f289a2db642c0bac
Sean2218 <monica.lin850@gmail.com>
2026-04-12 22:45:13 +0000

    Add API

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const api = require('./api')
-const fs = require('fs').promises
-const path = require('path')
 const express = require('express')
 
 // Set the port
@@ -10,32 +8,9 @@ const app = express()
 // Register the public directory
 app.use(express.static(__dirname + '/public'));
 // register the routes
-app.get('/products', listProducts)
-app.get('/', handleRoot);
+app.get('/products', api.listProducts)
+app.get('/', api.handleRoot);
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
 
-/**
- * Handle the root route
- * @param {object} req
- * @param {object} res
-*/
-function handleRoot(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
-}
-
-/**
- * List all products
- * @param {object} req
- * @param {object} res
- */
-async function listProducts(req, res) {
-  const productsFile = path.join(__dirname, 'data/full-products.json')
-  try {
-    const data = await fs.readFile(productsFile)
-    res.json(JSON.parse(data))
-  } catch (err) {
-    res.status(500).json({ error: err.message })
-  }
-}
 
