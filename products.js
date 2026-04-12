3025fb30c487b69b5d71c53b621ebdcf5e1a118a
Sean2218 <monica.lin850@gmail.com>
2026-04-12 22:37:50 +0000

    This function reads product data and returns

--- products.js
+++ products.js
@@ -11,7 +11,9 @@ module.exports = {
  * This function reads and returns all product data.
  * @returns {Promise<Array>}
  */
-async function list() {
+async function list(options = {}) {
+  const { offset = 0, limit = 25 } = options
   const data = await fs.readFile(productsFile)
-  return JSON.parse(data)
+
+  return JSON.parse(data).slice(offset, offset + limit)
 }
